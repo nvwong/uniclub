9ef4784ee116db79e95c558cf265cbe3fe793db6
翁治平 <gocreating@gmail.com>
2016-12-06 13:45:48 +0800

    Add user's normalizr schema

--- src/common/schemas/index.js
+++ src/common/schemas/index.js
@@ -3,3 +3,7 @@ import { Schema } from 'normalizr';
 export let todoSchema = new Schema('todos', {
   idAttribute: '_id',
 });
+
+export let userSchema = new Schema('users', {
+  idAttribute: '_id',
+});

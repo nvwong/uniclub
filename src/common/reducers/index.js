5dc6b34da93bffa8a382582973cd9490b231142b
翁治平 <gocreating@gmail.com>
2016-12-06 13:56:35 +0800

    Remove old version redux stuff

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -3,8 +3,6 @@ import routing from './routerReducer';
 import cookies from './cookieReducer';
 import errors from './errorReducer';
 import apiEngine from './apiEngineReducer';
-import pages from './pageReducer';
-import todos from './todoReducer';
 import form from './formReducer';
 import intl from './intlReducer';
 import entity from './entityReducer';
@@ -15,8 +13,6 @@ const rootReducer = combineReducers({
   cookies,
   errors,
   apiEngine,
-  pages,
-  todos,
   form, // must mount as `form` from redux-form's docs
   intl,
   entity,

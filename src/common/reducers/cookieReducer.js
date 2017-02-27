a6f044442a0a99de22bbe7490c85de8e5065ead8
翁治平 <gocreating@gmail.com>
2017-02-27 18:38:55 +0800

    Upgrade cookie actions and reducer

--- src/common/reducers/cookieReducer.js
+++ src/common/reducers/cookieReducer.js
@@ -1,9 +1,11 @@
 import cookie from 'cookie';
 import ActionTypes from '../constants/ActionTypes';
+import deserializeCookieMap from '../utils/deserializeCookieMap';
 
 let initCookies = {};
+
 if (process.env.BROWSER) {
-  initCookies = cookie.parse(document.cookie);
+  initCookies = deserializeCookieMap(cookie.parse(document.cookie));
 } else {
   initCookies = {};
 }
@@ -11,17 +13,9 @@ if (process.env.BROWSER) {
 export default (state = initCookies, action) => {
   switch (action.type) {
     case ActionTypes.SET_COOKIE: {
-      let cookiePair = {};
-      let value = action.cookie.value;
-      if (typeof action.cookie.value === 'string') {
-        value = action.cookie.value;
-      } else if (typeof action.cookie.value === 'object') {
-        value = JSON.stringify(action.cookie.value);
-      }
-      cookiePair[action.cookie.name] = value;
       return {
         ...state,
-        ...cookiePair,
+        [action.cookie.name]: action.cookie.value,
       };
     }
     default: {

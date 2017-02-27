a663f231cda48762c286651225ffe883e3a04a2a
翁治平 <gocreating@gmail.com>
2017-02-27 15:51:50 +0800

    Automatically redirect to corresponding page when specific errors occur

--- src/common/components/utils/ErrorList.js
+++ src/common/components/utils/ErrorList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { push } from 'react-router-redux';
 import isString from 'lodash/isString';
 import { connect } from 'react-redux';
 import Grid from 'react-bootstrap/lib/Grid';
 import Alert from 'react-bootstrap/lib/Alert';
 import Table from 'react-bootstrap/lib/Table';
 import { removeError } from '../../actions/errorActions';
+import ErrorCodes from '../../constants/ErrorCodes';
 
 function renderMetaContent(metaContent) {
   if (isString(metaContent)) {
@@ -52,17 +54,34 @@ function renderMeta(meta) {
 
 let ErrorList = ({ errors, dispatch }) => (
   <Grid>
-    {errors.map((error) => (
-      <Alert
-        key={error.id}
-        bsStyle="danger"
-        onDismiss={() => dispatch(removeError(error.id))}
-      >
-        <h4>{error.title}</h4>
-        {' ' + error.detail}
-        {error.meta && renderMeta(error.meta)}
-      </Alert>
-    ))}
+    {errors.map((error) => {
+      if ([
+        ErrorCodes.TOKEN_EXPIRATION,
+        ErrorCodes.BAD_TOKEN,
+      ].indexOf(error.code) >= 0) {
+        dispatch(removeError(error.id));
+        dispatch(push('/user/login'));
+      }
+      if ([
+        ErrorCodes.USER_UNAUTHORIZED,
+        ErrorCodes.PERMISSION_DENIED,
+      ].indexOf(error.code) >= 0) {
+        dispatch(removeError(error.id));
+        dispatch(push('/'));
+      }
+
+      return (
+        <Alert
+          key={error.id}
+          bsStyle="danger"
+          onDismiss={() => dispatch(removeError(error.id))}
+        >
+          <h4>{error.title}</h4>
+          {' ' + error.detail}
+          {error.meta && renderMeta(error.meta)}
+        </Alert>
+      );
+    })}
   </Grid>
 );
 

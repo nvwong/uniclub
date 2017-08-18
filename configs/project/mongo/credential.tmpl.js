6558cf2615428f517d7c876c979abff93d182942
Nv Wong <27146493+nvwong@users.noreply.github.com>
2017-08-18 11:14:24 +0800

    hi

--- configs/project/mongo/credential.tmpl.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  development: 'mongodb://<username>:<password>@<host>:<port>/<databaseName>-development',
-  test: 'mongodb://<username>:<password>@<host>:<port>/<databaseName>-test',
-  production: 'mongodb://<username>:<password>@<host>:<port>/<databaseName>-production',
-};

f2b49ce367c7011f96ecf116192e3ced0a9cdb4e
Nv Wong <27146493+nvwong@users.noreply.github.com>
2017-08-21 14:06:01 +0800

    recaptcha

--- .deploy/configs/project/mongo/credential.js
+++ .deploy/configs/project/mongo/credential.js
@@ -1,5 +1,5 @@
 module.exports = {
   development: 'mongodb://nv:123456@ds149353.mlab.com:49353/nv-test',
   test: 'mongodb://nv:123456@ds149353.mlab.com:49353/nv-test-2',
-  production: 'mongodb://nv:123456@ds149353.mlab.com:49353/nv-test-3',
+  production: 'mongodb://nv:123456@ds149433.mlab.com:49433/nv-test-3',
 };
